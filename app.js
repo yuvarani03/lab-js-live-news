6a4029d38f6281d9ea31c2496bef04f7dbd5c793
yuvarani03 <yuvarvh1998@gmail.com>
2022-01-20 23:59:20 +0530

    updated

---
+++ app.js
@@ -0,0 +1,33 @@
+let apikey="3de20187dd2244d884515e79bbadfa09";
+async function selCountry() {
+    let country = document.getElementById("country").value;
+    if (country !== "") {
+        let url = "https://newsapi.org/v2/top-headlines?country="+country+"&apiKey="+apikey;
+        axios.get(url)
+            .then(res => {
+                console.log(res)
+                let News = res.data.articles;
+                console.log(News);
+                var newsContent = '';
+                for (var i in News) {
+                    newsContent +=
+                    `<div class="newsContent">
+                    <img  src="${News[i].urlToImage}">
+                    <div class="title">
+                    <h6>${News[i].title}</h6>
+                    </div>
+                    <div class="des">
+                    <p>${News[i].description}</p>
+                    </div>
+                    <div class="btn">
+                    <a href="${News[i].url}">READ MORE</a>
+                    </div>
+                    </div>`;
+                }
+                document.getElementById('newscontent').innerHTML = newsContent;
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+}
